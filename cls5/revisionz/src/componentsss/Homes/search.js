af22ad5cbe2bd2da6e59d2b7b6c8053c354094bd
0tohero99 <shubhamsinghhindustan@gmail.com>
2024-04-11 07:20:26 -0700

    ok

--- cls5/revisionz/src/componentsss/Homes/search.js
+++ cls5/revisionz/src/componentsss/Homes/search.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 import "./search.css";
 
-const base_url = process.env.REACT_APP_Base_url;
-
+const base_url = process.env.REACT_APP_CITY_URL;
 class search extends Component {
   constructor() {
     console.log("constructor");
     super()
     this.state = {
       Location: " "
-    };
+    }
   }
   render() {
     console.log("render");
@@ -30,11 +29,15 @@ class search extends Component {
             <option>---Select  Restaurent---</option>
           </select>
         </div>
-
-       
       </div>
     )
   }
-}
+  componentDidMount() {
+    console.log("componentDidMount");
+    fetch(`${base_url}/location`, { method: "GET" })
+      .then((res) => console.log(res.json()))
+      .then((data) => console.log(data))
 
+  }
+}
 export default search;
